00dd31da60eb0c6dd36c7bbaad7bb7fedce68d81
Daniel Diaz <diaz.carrete@facebook.com>
2020-07-10 17:41:16 +0200

    still aeson...

--- lib-aeson/ByOtherNames/Aeson.hs
+++ lib-aeson/ByOtherNames/Aeson.hs
@@ -1,6 +1,11 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 module ByOtherNames.Aeson
   ( module ByOtherNames,
     JSON,
@@ -12,6 +17,8 @@ import Data.Aeson
 import Data.Aeson.Types
 import Data.Text
 import Data.Functor.Compose
+import Data.Kind
+import GHC.Generics
 
 data JSON = JSON
 
@@ -19,9 +26,14 @@ instance Rubric JSON where
     type AliasesType JSON = Text
 
 
-newtype FieldParser a = FieldFromJSON (Object -> Parser a) 
+newtype FieldParser a = FieldParser (Object -> Parser a) 
                       deriving (Functor,Applicative) via ((->) Object `Compose` Parser)
 
--- type FieldFromJSON :: Type -> Constraint
--- class FieldParser t where
---     fieldParser :: 
+type FieldFromJSON :: (Type -> Type) -> Constraint
+class FieldFromJSON t where
+    fieldParser :: Aliases Text t -> FieldParser (t x)
+
+-- instance FromJSON v => FieldFromJSON (S1 ('MetaSel (Just symbol) x y z) (Rec0 v)) where
+--     fieldParser (Leaf fieldName) = FieldParser \o -> explicitParseField parseJSON o fieldName
+
+
