946e97493c7cb37ffbefb7d06af0cbe45682b83d
Daniel Díaz <diaz_carrete@yahoo.com>
2022-07-18 22:33:51 +0200

    added doctest skeleton

---
+++ tests/doctests.hs
@@ -0,0 +1,9 @@
+module Main where
+import Test.DocTest
+main = doctest [
+      "-ilib"
+    , "lib/ByOtherNames.hs"
+    , "lib/ByOtherNames/Aeson.hs"
+    , "lib/ByOtherNames/TH.hs"
+    , "lib/ByOtherNames/Constraint.hs"
+    ]
